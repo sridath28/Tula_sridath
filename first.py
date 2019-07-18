9a1f13a6eac178ea5bde579ae9dfda89d84be1df
sridath28 <51149011+sridath28@users.noreply.github.com>
2019-07-18 12:06:44 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+a =int(input('enter the value'))
+if (a==0):
+    print('the given value is Zero')
+elif(a>=0):
+    print('the given value is positive')
+else:
+    print('the given value is negative')
+    
